624f3c07b941ac172bbed8c9dab2d86eada700f9
Layza Brandt <LayzaBrandt@hotmail.com>
2024-02-04 21:18:42 -0300

    Colocando as peças iniciais no tabuleiro

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -14,6 +14,11 @@ namespace xadrez
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 4));
+
+
             Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
--- xadrez/tabuleiro/Peca.cs
+++ xadrez/tabuleiro/Peca.cs
@@ -5,12 +5,12 @@ namespace tabuleiro
     public class Peca
     {
         public Posicao? posicao { get; set; }
-        public Cor cor { get; protected set; } //protected → só vai acessada por ela mesma e pela subclasses dela;
+        public Cor cor { get; protected set; } //protected → só vai ser acessada por ela mesma e pela subclasses dela;
         public int qteMovimentos {  get; protected set; }
         public Tabuleiro? tab {  get; protected set; }
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.tab = tab;
             this.cor = cor;
             this.qteMovimentos = 0;//a peça quando acaba de ser criada no construtor ela ainda não teve movimento, então inicia com 0;

385d4baf19f2838f38844e9d73fdec1e9a72ef5c
Layza Brandt <LayzaBrandt@gmail.com>
2024-02-18 13:37:00 -0300

    Excessões e novos métodos para identificar posicionamento das peças

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -12,16 +12,11 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 4));
-
-
-            Tela.imprimirTabuleiro(tab);
-
-            Console.ReadLine();
+            Console.WriteLine(pos.toPosicao());
+                Console.ReadLine();
         }
     }
 }
